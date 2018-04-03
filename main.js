92ef72845592be5e898484ed52163b794e88d0af
Tmanaswini <thotamanaswini97@gmail.com>
2018-04-03 16:33:35 +0530

    json

---
+++ main.js
@@ -0,0 +1,45 @@
+
+function loadJson(file,callback)
+{
+      var xhr=new XMLHttpRequest();
+      xhr.overrideMimeType("application/json");
+      xhr.open("GET",file,true);
+      xhr.onreadystatechange=function()
+      {
+          if(xhr.readyState ===4 && xhr.status =="200")
+          {
+              callback(xhr.responseText);
+          }
+      };
+      xhr.send();
+}
+loadJson("data.json",function(text)
+{
+  let data=JSON.parse(text);
+    console.log(data);
+carrerobjective(data.carrerobjective);
+education(data.education);
+})
+var  right=document.querySelector(".content");
+function carrerobjective(car)
+{
+    console.log(car.info);
+    var h3=document.createElement("h3");
+    h3.textContent="carrer objective";
+    right.appendChild(h3);
+    var hr=document.createElement("hr");
+    right.appendChild(hr);
+    var p=document.createElement("p");
+    p.textContent=car.info;
+      right.appendChild(p);
+}
+function education(edu)
+{
+  var h3=document.createElement("h3");
+  h3.textContent="EDUCATION";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  h3.textContent="EDUCATION";
+  right.appendChild(hr);
+
+}
